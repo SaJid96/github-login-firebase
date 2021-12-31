8ae359d5e920489c0253cc55d31ba54901691fd6
Sajid Hussain <sajidhussain98@gmail.com>
2021-12-31 16:04:10 +0530

    added github service api

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+// https://api.github.com/users/SaJid96
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  constructor( private http:HttpClient) { }
+
+
+  getUserDetails(userName:string){
+    return this.http.get(`https://api.github.com/users/${userName}`);
+  }
+
+  getRepos(repoUrl:string){
+    return this.http.get(repoUrl)
+  }
+
 }
